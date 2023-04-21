6a26c92cb5d05d55eb44a1fa7bd19844c23dc627
Kenneth Tan <kennethtxytqw@gmail.com>
2023-04-22 03:02:56 +0800

    updated favicon

--- site.config.ts
+++ site.config.ts
@@ -26,7 +26,7 @@ export default siteConfig({
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: null,
+  defaultPageIcon: "https://i.ibb.co/pRLdP7T/yyk-robot.webp",
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
